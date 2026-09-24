3ca545388b62354a610852972dba4c10c6abbffb
Tanya Maheshwari <150817230+tanyamaheshwari15@users.noreply.github.com>
2026-09-24 16:27:16 +0530

    Time: 38 ms (5.01%), Space: 110.5 MB (9.29%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int e: nums){
+            map.put(e, map.getOrDefault(e,0)+1);
+        }
+
+        for(Map.Entry<Integer, Integer> e: map.entrySet()){
+            if(e.getValue() > 1)
+                return true;
+        }
+
+        return false;
+    }
+}
