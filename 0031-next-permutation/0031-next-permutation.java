d3053b6f2bc28b891c30738a57c8a3c9f5158dee
Tanya Maheshwari <150817230+tanyamaheshwari15@users.noreply.github.com>
2026-09-25 18:44:18 +0530

    Time: 0 ms (100%), Space: 44.7 MB (32.85%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.java
@@ -0,0 +1,47 @@
+class Solution {
+
+    public static void reverseFromIndex(int[] arr, int startIndex) {
+        int left = startIndex;
+        int right = arr.length - 1;
+        
+        while (left < right) {
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            
+            left++;
+            right--;
+        }
+    }
+
+    public void nextPermutation(int[] nums) {
+        int size = nums.length-1;
+        int min = Integer.MAX_VALUE;
+        int index = -1;
+        int pivot = 0;
+
+        for(int i = size-1; i >= 0; i--){
+            
+            if(nums[i] < nums[i+1]){
+
+                for(int j = size; j > i; j--){
+                    if(nums[j] < min && nums[j] > nums[i]){ 
+                        min = nums[j];
+                        index = j;
+                    }  
+                }
+
+                if(nums[i] < min){
+                    int temp = nums[index];
+                    nums[index] = nums[i];
+                    nums[i] = temp;
+                    reverseFromIndex(nums, i+1);
+                    pivot = 1;
+                }
+
+                break;
+            }
+        }
+        if(pivot == 0) reverseFromIndex(nums, 0);
+    }
+}
